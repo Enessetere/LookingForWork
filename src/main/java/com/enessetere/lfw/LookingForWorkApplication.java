f425cecf4c105d3d7e9cd7630592f56a373be4a6
Enessetere <muszalski.sebastian@gmail.com>
2021-09-03 22:32:38 +0200

    build: start up

---
+++ src/main/java/com/enessetere/lfw/LookingForWorkApplication.java
@@ -0,0 +1,13 @@
+package com.enessetere.lfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingForWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookingForWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enessetere/lfw/LookingForWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enessetere.lfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingForWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
