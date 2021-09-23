9c648737be6e7dd6907cd9189ec1a5d78a2a809a
Enessetere <muszalski.sebastian@gmail.com>
2021-09-23 22:04:50 +0200

    feat: add note subclass for review

---
+++ src/main/java/com/enessetere/lfw/review/Note.java
@@ -0,0 +1,41 @@
+package com.enessetere.lfw.review;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Setter(value = AccessLevel.PRIVATE)
+public class Note {
+    private Map<String, Short> notes;
+    private double average;
+
+    private Note() {
+    }
+
+    public static class Builder {
+        private final Note note;
+        public Builder() {
+            this.note = new Note();
+        }
+
+        public Builder notes(Map<String, Short> notes) {
+            note.setNotes(notes);
+            return this;
+        }
+
+        public Note build() {
+            short notesSum = 0;
+            short count = 0;
+            for (Short noteValue : note.notes.values()) {
+                if (Objects.nonNull(noteValue)) {
+                    notesSum += noteValue;
+                    count++;
+                }
+            }
+            this.note.setAverage(notesSum/(double)count);
+            return note;
+        }
+    }
+}
